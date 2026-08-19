07cdc672e7ff3197b991cb926cca2112e4c52827
Gideon Tobing <gideon.tobing@gmail.com>
2026-08-19 21:35:43 +0800

    add topbar red with white text

--- app/src/main/java/com/gt/pokedex/MainActivity.kt
+++ app/src/main/java/com/gt/pokedex/MainActivity.kt
@@ -5,12 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,16 +24,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PokedexHome() {
     Column() {
-        PokedexAppBar()
-    }
-}
+        PokedexAppBar({
 
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun PokedexAppBar() {
-    TopAppBar(title = {
-        (Text("PokeBar"))
-    })
+        })
+    }
 }
 
 
---
+++ app/src/main/java/com/gt/pokedex/PokedexAppBar.kt
@@ -0,0 +1,39 @@
+package com.gt.pokedex
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.gt.pokedex.ui.theme.PokedexTheme
+
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun PokedexAppBar(onActionClick: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.pokedex_title),
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }, colors = TopAppBarDefaults.topAppBarColors().copy(
+            containerColor = Color.Red
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PokedexAppBarPreview() {
+    PokedexTheme {
+        PokedexAppBar(onActionClick = {})
+    }
+}
